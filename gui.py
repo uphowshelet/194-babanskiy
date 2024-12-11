9f310652159eff5d8b008d888f85e9db20a0ac15
uphowshelet <babanskynikita@yandex.ru>
2024-12-12 00:52:08 +0700

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox
+from lab1_defs import (
+    get_first_50_national_names, get_first_50_state_names,
+    add_national_name, add_state_name,
+    get_national_name_by_id, get_state_name_by_id,
+    update_national_name, update_state_name,
+    delete_national_name, delete_state_name,
+    search_national_names_by_name, search_state_names_by_name
+)
